d5169ad5067a8f4488c936e5875d871f4342d606
Claudena Daniel <cjdaniel0530@gmail.com>
2026-06-01 08:57:40 -0400

    fixed abilities name in array and added textContent to loop

--- scriptSba.js
+++ scriptSba.js
@@ -6,7 +6,7 @@ const Gen1poke = [
  name: "",
  type: "",
  level: ,
- abilitiies: ""
+ abilities: ""
 
 }
 ]
@@ -31,7 +31,7 @@ const Gen1poke = [
  name: "Bulbasaur",
  type: "Grass/Poison",
  level: 24,
- abilitiies: "Overgrow/Chlorophyll",
+ abilities: "Overgrow/Chlorophyll",
  image:""
 
 },
@@ -39,7 +39,7 @@ const Gen1poke = [
  name: "Charmander",
  type: "Fire",
  level: 25,
- abilitiies: "Blaze/Solar Power",
+ abilities: "Blaze/Solar Power",
  image:""
 
 },
@@ -47,7 +47,7 @@ const Gen1poke = [
  name: "Squirtle",
  type: "Water",
  level: 18,
- abilitiies: "Torrent/Rain Dish",
+ abilities: "Torrent/Rain Dish",
  image:""
 
 },
@@ -55,7 +55,7 @@ const Gen1poke = [
  name: "Caterpie",
  type: "Bug",
  level: 13,
- abilitiies: "Shield Dust/Run Away",
+ abilities: "Shield Dust/Run Away",
  image:""
 
 },
@@ -63,7 +63,7 @@ const Gen1poke = [
  name: "Pidgey",
  type: "Normal/Flying",
  level: 20,
- abilitiies: "Keen Eye/Tangled Feet/Bug Pecks",
+ abilities: "Keen Eye/Tangled Feet/Bug Pecks",
  image:""
 
 },
@@ -71,56 +71,56 @@ const Gen1poke = [
  name: "Rattata",
  type: "Normal",
  level: 15,
- abilitiies: "Run Away/Guts/Hustle"
+ abilities: "Run Away/Guts/Hustle"
 
 },
 { id: 27,
  name: "Sandshrew",
  type: "Ground",
  level: 21,
- abilitiies: "Sand Veil/Sand Rush"
+ abilities: "Sand Veil/Sand Rush"
 
 },
 { id: 29,
  name: "Nidoran(F)",
  type: "Poison",
  level: 22,
- abilitiies: "Poison Point/Rivalry/Hustle"
+ abilities: "Poison Point/Rivalry/Hustle"
 
 },
 { id: 32,
  name: "Nidoran(M)",
  type: "Posion",
  level: 22,
- abilitiies: "Poison Point/Rivalry/Hustle"
+ abilities: "Poison Point/Rivalry/Hustle"
 
 },
 { id: 35,
  name: "Clefairy",
  type: "Fairy",
  level: 25,
- abilitiies: "Cute Charm/Magic Guard/Friend Guard"
+ abilities: "Cute Charm/Magic Guard/Friend Guard"
 
 },
 { id: 37,
  name: "Vulpix",
  type: "Fire",
  level: "26",
- abilitiies: "Flash Fire/Drought"
+ abilities: "Flash Fire/Drought"
 
 },
 { id: 52,
  name: "Meowth",
  type: "Normal",
  level: 17,
- abilitiies: "Pick Up/Technician/Unnerve"
+ abilities: "Pick Up/Technician/Unnerve"
 
 },
 { id: 54,
  name: "Psyduck",
  type: "Water",
  level: "22",
- abilitiies: "Damp/Cloud Nine/Swift Swim"
+ abilities: "Damp/Cloud Nine/Swift Swim"
 
 },
 //current count 14 pokemon
@@ -128,42 +128,42 @@ const Gen1poke = [
  name: "Gyrados",
  type: "Water/Flying",
  level: 26,
- abilitiies: ""
+ abilities: ""
 
 },
 { id: 133 ,
  name: "Eevee",
  type: "Normal",
  level: 24,
- abilitiies: "Run Away/Adaptability/Anticipation"
+ abilities: "Run Away/Adaptability/Anticipation"
 
 },
 { id: 147 ,
  name: "Dratini",
  type: "Dragon",
  level: 21,
- abilitiies: "Shed Skin/Marvel Scale"
+ abilities: "Shed Skin/Marvel Scale"
 
 },
 { id: 106,
  name: "Hitmonlee",
  type: "Fighting",
  level: 27,
- abilitiies: "Limber/Reckless/Unburden"
+ abilities: "Limber/Reckless/Unburden"
 
 },
 { id: 56,
  name: "Mankey",
  type: "Fighting",
  level: 24,
- abilitiies: "Vital Spirit/Anger Point/Defiant"
+ abilities: "Vital Spirit/Anger Point/Defiant"
 
 },
 { id: 74,
  name: "Geodude",
  type: "Rock/Ground",
  level: 21,
- abilitiies: "Rock Head/Sturdy/Sand Veil"
+ abilities: "Rock Head/Sturdy/Sand Veil"
 
 }
 ]
@@ -191,27 +191,41 @@ const pokemonContainer = document.getElementById("pokemonContainer");
 const pokemonList = document.getElementById("pokeList");
 const pokemonCard = document.getElementById("pokeCards");
 
+const ability = pokemon.abilities.split("/");
 // loop through pokemon database to display it 
 Gen1poke.forEach((pokemon) =>{
    // need to create a pokemon info card
     
     const card = document.createElement("div");
-    name.textContent = pokemon.name;
+    
 
     const name = document.createElement("h2");
-    name.textContent
+    name.textContent = pokemon.name;
 
     const type = document.createElement("p");
+    type.textContent = `Type: ${pokemon.type}`;
 
     const level = document.createElement("p");
+    type.textContent = `Level: ${pokemon.level}`;
 
     const abilities = document.createElement("p");
-
+    type.textContent = `Abilities: ${pokemon.abilities}`;
+    
     card.append(name, type, level, abilities);
 
     container.appendChild(card)
     });
+// need a button to click through each pokemon
 
+    Gen1poke.forEach((pokemon) => {
+        const button = document.createElement("button");
+
+        button.textContent = pokemon.name;
+
+        button.addEventListener("click", () =>{
+            
+        })
+    })
    
     
 
